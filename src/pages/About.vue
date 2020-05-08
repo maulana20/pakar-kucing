f1296ef9d00529ff3f12685dbc3eddc0931fd926
maulana <maulana20@versatech.co.id>
2020-05-08 16:02:26 +0700

    revisi tentang
    Signed-off-by: maulana <maulana20@versatech.co.id>

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,12 @@
 <template>
 	<div class="content">
 		<div class="content__info">
-			About
+			<div class="content__circle">
+				<b-img center src="https://avatars1.githubusercontent.com/u/11070656?s=460&v=4" rounded="circle" alt="Maulana Saputra" width="120px"></b-img>
+			</div>
+			<b-card title="Maulana Saputra" sub-title="Web Developer">
+				<b-card-text>Sistem pakar untuk mendiagnosa penyakit kulit kucing di sertai terapi penyembuhan dan cara pengobatannya.</b-card-text>
+			</b-card>
 		</div>
 	</div>
 </template>
@@ -15,6 +20,9 @@
 		margin: 60px 0 20px 0;
 		&__info {
 			padding: 10px 0 10px 0;
+		}
+		&__circle {
+			padding: 10px 0 20px 0;
 		}
 	}
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="content">
 		<div class="content__image"><b-img src="img/cat.gif" /></div>
+		<div class="content__info">𝒫𝒶𝓀𝒶𝓇 𝒦𝓊𝒸𝒾𝓃𝑔, 𝒷𝓊𝓀𝒶𝓃 𝓀𝓊𝒸𝒾𝓃𝑔 𝓀𝓊𝒸𝒾𝓃𝑔𝒶𝓃</div>
 	</div>
 </template>
 <script>
@@ -14,6 +15,11 @@
 		&__image {
 			padding: 10px 0 10px 0;
 			text-align: center;
+		}
+		&__info {
+			padding: 5px 0 10px 0;
+			text-align: center;
+			font-size: 20px;
 		}
 	}
 </style>
