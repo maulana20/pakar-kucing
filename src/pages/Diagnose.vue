b39b23b5ef9ae621f973fd61464b3799b8cc357c
maulana <maulana20@versatech.co.id>
2020-05-09 14:40:26 +0700

    revisi diagnosa
    Signed-off-by: maulana <maulana20@versatech.co.id>

--- src/pages/Diagnose.vue
+++ src/pages/Diagnose.vue
@@ -1,8 +1,8 @@
 <template>
-	<b-modal ref="diagnose-modal" hide-footer centered :title="position" hide-header-close no-close-on-backdrop>
-		<div class="d-block">{{ question }}</div>
-		<b-button size="sm" class="float-right" @click="is_last ? diagnoseClose() : diagnoseOpen(index)">TIDAK</b-button>
-		<b-button size="sm" class="float-right" variant="success" @click="is_last ? diagnoseClose() : diagnoseOpen(index)" style="margin-right:6px;">YA</b-button>&nbsp;
+	<b-modal ref="diagnose-modal" hide-footer centered :title="diagnose.position" hide-header-close no-close-on-backdrop>
+		<div class="d-block">{{ diagnose.question }}</div>
+		<b-button size="sm" class="float-right" @click="diagnoseNext(diagnose.data, 'NO', index)">TIDAK</b-button>
+		<b-button size="sm" class="float-right" variant="success" @click="diagnoseNext(diagnose.data, 'YES', index)" style="margin-right:6px;">YA</b-button>&nbsp;
 	</b-modal>
 </template>
 <script>
@@ -11,8 +11,13 @@
 		data: function() {
 			return {
 				index: 0,
-				position: '',
-				question: '',
+				count: 0,
+				diagnose_list: [],
+				diagnose: {
+					position: '',
+					question: '',
+					data: {}
+				},
 				is_last: false
 			}
 		},
@@ -26,23 +31,38 @@
 			initData: function() {
 				this.index = 0
 				this.is_last = false
+				this.diagnose_list = []
 			},
 			diagnoseOpen: function(index) {
 				var gejala_list = this.$store.getters.gejalaList
+				
+				this.count = gejala_list.length
 				this.index = index + 1
 				
-				this.position = 'DIAGNOSA KE ' + this.index + '/' + gejala_list.length
-				this.question = gejala_list[index].nm_gejala
+				this.diagnose = {
+					position: 'DIAGNOSA KE ' + this.index + '/' + this.count,
+					question: gejala_list[index].nm_gejala + ' ?',
+					data: gejala_list[index]
+				}
+				
 				this.$refs['diagnose-modal'].show()
 				this.$root.$emit('is_footer', true)
-				
-				if (this.index == gejala_list.length) this.is_last = true
 			},
-			diagnoseClose: function() {
-				this.initData()
-				
-				this.$refs['diagnose-modal'].hide()
-				this.$root.$emit('is_conclusion', true)
+			diagnoseNext: function(data, option, index) {
+				if (this.is_last == true) {
+					if (option == 'YES') this.diagnose_list.push(data)
+					
+					this.$refs['diagnose-modal'].hide()
+					this.$root.$emit('is_conclusion', true)
+					console.log(this.diagnose_list)
+					
+					this.initData()
+				} else {
+					if (option == 'YES') this.diagnose_list.push(data)
+					
+					this.diagnoseOpen(index)
+					if (this.index == this.count) this.is_last = true
+				}
 			}
 		}
 	}
